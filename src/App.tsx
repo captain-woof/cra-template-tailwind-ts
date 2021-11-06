ea06a13b8f05415de47e3338d5369d1d33a71d39
Sohail Saha <captainwoof.official@gmail.com>
2021-11-06 17:39:02 +0530

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1 className="text-green-400 text-xl">Tailwind is awesome!</h1>
+    </div>
+  );
+}
+
+export default App;
